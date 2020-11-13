3459af105ad7d3a28d625e60423cec12ea9b18de
Rasha Rahman <rasharahman@rashas-mbp.lan>
2020-11-12 16:52:26 -0800

    const change remove file

--- pages/gameOver.js
+++
@@ -1,3 +0,0 @@
-export const GameOver = () => {
-    <ShowEnd opts={opts} setCurAct={setCurAct} history={history} time={finalTime} sameBG={sameBG} levelRestart={levelRestart} showEnd={showEnd} />
-}
